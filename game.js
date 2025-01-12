26cd2b68c00173d38bc4e5de18a3f8e731cca2e0
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-12 18:07:16 +0800

    Add click event listeners to all buttons

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
-// test if external js file is working
-const test = document.querySelector("h1");
+// First player has the 'X' marker
+let marker = "X";
 
-test.addEventListener("click", () => {
-	alert("Hi");
-});
+// Select all buttons
+const buttons = document.querySelectorAll("button");
+
+// Add click event listeners to all buttons to mark the board
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", function () {
+		
+	});
+}
