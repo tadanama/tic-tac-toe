3c04be6ba3ea5cffb04f14e35feac6fa73339413
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-12 18:11:27 +0800

    Write a function to enable user to mark the board
    - User can only mark the board when the selected cell is empty
    - After user has mark the board, change the marker for the other player's turn
    - When the user selects a cell that is not empty, display a message

--- game.js
+++ game.js
@@ -7,6 +7,25 @@ const buttons = document.querySelectorAll("button");
 // Add click event listeners to all buttons to mark the board
 for (let i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function () {
-		
+		makeMove(this);
 	});
+}
+
+function makeMove(currentButton) {
+	// Check if the tic tac toe cell is empty
+	if (currentButton.textContent === "") {
+		// Mark the board
+		currentButton.textContent = marker;
+
+		// Check if the user won
+		// Else change the marker
+		changeMarker();
+	} else {
+		// Create a p element to display message
+		const message = document.createElement("p");
+		// Set the message
+		message.textContent = "Select an empty cell.";
+		// Append the message element to the h1
+		document.querySelector("h1").appendChild(message);
+	}
 }
